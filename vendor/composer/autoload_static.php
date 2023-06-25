62cd22c3e1884fd986251b0a3a1ff3dd5cb1db93
Dmitrii Vlasov <ring.72.dmitry@yandex.ru>
2023-06-25 17:04:29 +0200

    Create .htaccess

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit611b3f61020147da8df197aa4da30cc5
 {
+    public static $prefixLengthsPsr4 = array (
+        'A' => 
+        array (
+            'App\\' => 4,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'App\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/app',
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
@@ -13,6 +27,8 @@ class ComposerStaticInit611b3f61020147da8df197aa4da30cc5
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit611b3f61020147da8df197aa4da30cc5::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit611b3f61020147da8df197aa4da30cc5::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInit611b3f61020147da8df197aa4da30cc5::$classMap;
 
         }, null, ClassLoader::class);
